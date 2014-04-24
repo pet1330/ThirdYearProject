ad2be176f553f366e87775988806ea75da31ddf9
Peter Lightbody <pet1330@gmail.com>
2014-04-24 20:35:36 +0100

    final test minus excel implementation

--- Methods/combinationMaker.m
+++ Methods/combinationMaker.m
@@ -1,18 +1,27 @@
 function combinationMaker
 v = [0 1 2 3 4 5];
-p = perms(v);
-hist(p);
-[s,~] = size(p);
-fileNum=1;
-fileID = fopen(sprintf('combo/COMBO%d',fileNum),'w');
-for elm = 1:s
-    fprintf(fileID,'%d%d%d%d%d%d%d%d%d%d\n',p(elm,:));
-    if(mod(elm,60480)==0)
-        fclose(fileID);
-        fileNum = fileNum+1;
-        fileID = fopen(sprintf('combo/COMBO%d',fileNum),'w');
+fileID = fopen('combo.perms','w');
+while(~isempty(v))
+    p = perms(v);
+    [s,z] = size(p);
+    for elm = 1:s
+        switch(z)
+            case 1
+                fprintf(fileID,'%d\n',p(elm,:));
+            case 2
+                fprintf(fileID,'%d%d\n',p(elm,:));
+            case 3
+                fprintf(fileID,'%d%d%d\n',p(elm,:));
+            case 4
+                fprintf(fileID,'%d%d%d%d\n',p(elm,:));
+            case 5
+                fprintf(fileID,'%d%d%d%d%d\n',p(elm,:));
+            case 6
+                fprintf(fileID,'%d%d%d%d%d%d\n',p(elm,:));
+        end
     end
+    disp(s);
+    v = v(1:length(v)-1);
 end
 fclose(fileID);
-disp(s);
 end
